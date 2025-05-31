b75b8b7c799b07e5f78e7e31b77cd2f1611a27cc
Junior Rodrigues <rodriguesx@proton.me>
2025-05-31 10:56:11 -0300

    entendendo impl em structs

--- benchmark/src/main.rs
+++ benchmark/src/main.rs
@@ -1,69 +1,40 @@
-use rust_decimal::dec;
-use std::time::{SystemTime};
-
-use chrono::offset::Utc; 
-use chrono::DateTime;
-
-
-struct Color(i32, i32, i32, i32);
-
-
+#[derive(Debug)]
 struct Rectangle {
     length: u32,
     width: u32,
 }
 
-struct Customer { 
-    fullname: String,
-    email: String,
-    active: bool,
-    amount: rust_decimal::Decimal
-}
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.length * self.width
+    }
 
-struct Log {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.length > other.length && self.width > other.width
+    }
 
-    text: String,
-    moment: SystemTime
+    fn square(size: u32) -> Rectangle {
+        Rectangle { length: size, width: size }
+    }
 
 }
 
 fn main() {
-    //let customer = get_user();
-    //let log = get_log(SystemTime::now(), &customer.email);
-    //println!("Log: {} at: {:?}", log.text, format!("{}", Into::<DateTime<Utc>>::into(log.moment).timestamp()));
-
+    //let rect1 = Rectangle { length: 50, width: 30 };
 
     let rect1 = Rectangle { length: 50, width: 30 };
+    let rect2 = Rectangle { length: 40, width: 10 };
+    let rect3 = Rectangle { length: 45, width: 60 };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
+
+    let new_rec = Rectangle::square(30);
+
+    println!("{:?}", new_rec);
 }
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.length * rectangle.width
-}
-
-
-fn get_user() -> Customer {
-
-    return Customer {
-            fullname: String::from("Edson Rodrigues"),
-            email: String::from("email@email.com"),
-            active: true,
-            amount: dec!(3.45)
-
-        };
-
-}
-
-fn get_log(moment: SystemTime, customer_mail: &String) -> Log {
-    return Log { text: String::from(format!("Criando usuario com email: {customer_mail}")), moment }
-}
-
-
-// String interpolation sample
-// apples = 4
-// print("I have ${apples} apples.") # string interpolation
-// print("I have " + apples + " apples.") # string concatenation
-// print("I have %s apples.", apples) # format string
